ab2f1c3ae956cad2d504528376e97718b5d6473c
s8508235 <s8508235@yahoo.com.tw>
2024-04-24 20:41:53 +0800

    build: add gatus for monitoring and paperless-ngx for document management

--- backup.sh
+++ backup.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
-docker exec -t firefly_db_1 pg_dumpall -c -U firefly > dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
+file_name="dump_$(date +%d-%m-%Y"_"%H_%M_%S).sql"
+#docker exec -t firefly-setting-db-1 pg_dumpall -c -U firefly > dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
+docker exec -t firefly-setting-db-1 pg_dumpall -c -U firefly > backup/${file_name}
+echo "$(pwd)/backup/${file_name}"
 # restore
 # cat your_dump.sql | docker exec -i firefly_db_1 psql -U firefly
